63e104238bf00b4b55eaa2167217ae3783ac433b
repen7ant <ilyarepentant@icloud.com>
2026-05-31 12:25:00 +0500

    fix: change systemd-boot to grub

--- hosts/common.nix
+++ hosts/common.nix
@@ -6,7 +6,11 @@
   ];
 
   # --- Boot ---
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    device = "nodev";
+  };
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
