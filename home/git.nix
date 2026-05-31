d753dfb617647814d38440a9ec149ced40d8d1b9
repen7ant <ilyarepentant@icloud.com>
2026-06-01 00:16:42 +0500

    fix: git lfs

--- home/git.nix
+++ home/git.nix
@@ -1,12 +1,21 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.git = {
     enable = true;
-    lfs.enable = true;
-    settings.user = {
-      name = "repen7ant";
-      email = "ilyarepentant@icloud.com";
+    settings = {
+      user = {
+        name = "repen7ant";
+        email = "ilyarepentant@icloud.com";
+      };
+      filter.lfs = {
+        clean = "git-lfs clean -- %f";
+        smudge = "git-lfs smudge -- %f";
+        process = "git-lfs filter-process";
+        required = true;
+      };
     };
   };
+
+  home.packages = [ pkgs.git-lfs ];
 }
