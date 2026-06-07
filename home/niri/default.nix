36afa6503fb8b2b55b5f2de1fea5a295f30c0de2
repen7ant <ilyarepentant@icloud.com>
2026-06-07 16:57:14 +0500

    fix: niri symlink

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -5,4 +5,7 @@
 
   xdg.configFile."niri/config.kdl".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/niri/config.kdl";
+
+  xdg.configFile."niri/colors.kdl".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/niri/colors.kdl";
 }
